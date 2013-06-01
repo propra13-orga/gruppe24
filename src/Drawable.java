99e9ed4f3db9682edbfa5cfb92f4d07870e3a320
Tobias Jack <tobias.jack@uni-duesseldorf.de>
2013-06-01 16:00:42 +0200

    cleanup

--- src/Drawable.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Graphics;
-
-public interface Drawable {
-
-	public void drawObjects(Graphics g);
-	
-}
--- src/Movable.java
+++
@@ -1,7 +0,0 @@
-
-public interface Movable {
-
-	public void doLogic(long delta);
-	
-	public void move(long delta);
-}
